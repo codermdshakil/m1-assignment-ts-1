8ad758c2e1962c5afd8e48589c10dfd96e31f0c1
codermdshakil <ahmedshakil0512@gmail.com>
2025-05-08 00:27:21 +0600

    processValue is done

---
+++ src/processValue.ts
@@ -0,0 +1,16 @@
+
+function processValue(value: string | number): number{
+    if(typeof value === 'string'){
+        const valueLength = value.length;
+        console.log(`Output:`,valueLength);
+        return valueLength
+    }
+    else{
+        console.log(`Output:`,value*2);
+        return value*2;
+    }
+}
+processValue("hello");          // Output: 5
+processValue(10);               // Output: 20
+processValue("Shakil Ahmed");   // Output: 12
+processValue(50);               // Output: 100
